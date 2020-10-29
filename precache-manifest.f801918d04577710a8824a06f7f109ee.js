7561184bd663eb590b0400311c70feb4b9b546ba
Muhammad Sameem <43237227+sameem420@users.noreply.github.com>
2020-10-29 18:36:33 +0500

    Updates

---
+++ precache-manifest.f801918d04577710a8824a06f7f109ee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26c675dda1968aee0b4e5dae39c96953",
+    "url": "/REACTTodoApp/index.html"
+  },
+  {
+    "revision": "e35f945d4c2aa3d583aa",
+    "url": "/REACTTodoApp/static/css/2.9a9a1f6f.chunk.css"
+  },
+  {
+    "revision": "c54281626798d9c056ab",
+    "url": "/REACTTodoApp/static/css/main.1cd1021e.chunk.css"
+  },
+  {
+    "revision": "e35f945d4c2aa3d583aa",
+    "url": "/REACTTodoApp/static/js/2.824a296b.chunk.js"
+  },
+  {
+    "revision": "9a9e56582fbb6743dfc4c1baab3b2d6f",
+    "url": "/REACTTodoApp/static/js/2.824a296b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c54281626798d9c056ab",
+    "url": "/REACTTodoApp/static/js/main.19229709.chunk.js"
+  },
+  {
+    "revision": "f6a4283cf6247dcef823",
+    "url": "/REACTTodoApp/static/js/runtime-main.4533fc81.js"
+  }
+]);
